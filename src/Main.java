99bcaf774fb416df6b4a738c2eb31289c36fcf0d
Rey Enmanuel Marte Picherdo <email>
2026-06-03 17:50:55 -0400

    Estructuracion del menu

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,29 @@
+import java.util.Scanner;
 public class Main {
+    static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int b = 0;
+
+        do{
+            System.out.println("===================================================");
+            System.out.println("           .::REGISTRO DE USUARIOS::.              ");
+            System.out.println("===================================================");
+            System.out.println("1. Registrar Usuario");
+            System.out.println("2. Salir");
+
+            System.out.print("Ingrese la opcion deseada: ");
+            b = sc.nextInt();
+            sc.nextLine();
+
+            switch(b) {
+                case 1:
+                    break;
+                case 2:
+                    System.out.println("Saliendo del programa...");
+                    break;
+            }
+
+        }while(b != 2);
     }
     
 }
