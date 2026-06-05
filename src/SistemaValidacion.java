e8fcbfa165b01676a07577985e7e67d7969334ad
Rey Enmanuel Marte Picherdo <email>
2026-06-04 21:50:28 -0400

    Creacion del metodo ValidarUsuario, con las excepciones posibles y el mensaje que explica el error

--- src/SistemaValidacion.java
+++ src/SistemaValidacion.java
@@ -1,6 +1,11 @@
 
 import java.util.Scanner; 
 public class SistemaValidacion {
+    private NombreInvalidoException n = null;
+    private EdadInvalidaException e = null;
+    private CorreoInvalidoException c = null;
+    private SalarioInvalidoException s = null;
+
     Scanner scanner = new Scanner(System.in);
     Usuario[] BancoDatos = new Usuario[100];
     int TotalUsuarios = 0;
@@ -32,18 +37,46 @@ public class SistemaValidacion {
             double Salario = scanner.nextDouble();
             usuario.setSalario(Salario);
             System.out.println();
+            ValidarUsuario(Nombre, Correo, Edad, Salario);
 
             BancoDatos[TotalUsuarios] = usuario;
             TotalUsuarios++;
-        }catch () {
+        }catch (NombreInvalidoException n) {
+            System.out.println("Error al registrar al usuario: " + n.getMessage());
+        }catch(EdadInvalidaException e) {
+            System.out.println("Error al registrar al Usuario" + e.getMessage());
+        }catch (CorreoInvalidoException c) {
+            System.out.println("Error al registrar al Usuario: " + c.getMessage());
+        }catch (SalarioInvalidoException s){
+            System.out.println("Error al registrar al Usuario: " + s.getMessage());
+        }finally {
 
-        } catch() {
-
-        } catch () {
-
-        } finally {
-
+            System.out.println("Proceso finalizado...");
         }
         
+    }
+
+    public void ValidarUsuario (String Nombre, String Correo, int Edad, double Salario) throws NombreInvalidoException, EdadInvalidaException, CorreoInvalidoException, SalarioInvalidoException{
+        if(Nombre.isEmpty()){
+            throw new NombreInvalidoException("El nombre no puede estar vacio. Que se sepa, no te registraron sin nombre.");
+        } else if(Nombre.length() < 3) {
+            throw new NombreInvalidoException("El nombre debe tener al menos 3 caracteres");
+        }
+
+        if(Edad < 18){
+            throw new EdadInvalidaException("La edad debe ser mayor o igual a 18 años");
+        } else if(Edad > 100){
+            throw new EdadInvalidaException("La edad no puede ser mayor a 100 años. Ni que fueras momia, animal");
+        }
+
+        if(!Correo.contains("@")){
+            throw new CorreoInvalidoException("El correo electronico no es valido");
+        } else if(!Correo.contains(".")){
+            throw new CorreoInvalidoException("El correo electronico no es valido");
+        }
+
+        if (Salario < 0) {
+            throw new SalarioInvalidoException("El salario debe ser mayor que 0");
+        }
     }
 }
